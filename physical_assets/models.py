86306912283a19f0ef32e92c8ee15e87734a53f1
Abhishek Pareek <makeittotop@gmail.com>
2015-02-18 14:25:05 +0400

    Starting to add data models for the 'physical_assets' app

--- physical_assets/models.py
+++ physical_assets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Asset(model.Model):
+    pass
+
+class Asset_Detail(model.Model):
+    pass
+    
+class Purchase_Detail(model.Model):
+    pass
+    
+class Computer_Asset(model.Model):
+    pass
+                
