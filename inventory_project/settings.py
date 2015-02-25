606083576e081e11bb5cf10a7bb75af2d1a48333
Abhishek Pareek <makeittotop@gmail.com>
2015-02-25 18:43:55 +0400

    Using postgresdb instead of sqlite3. Database name is inventory.

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -62,12 +62,25 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inventory',
+        'USER': 'abhishek',
+        'PASSWORD': 'qwerty',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}    
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
